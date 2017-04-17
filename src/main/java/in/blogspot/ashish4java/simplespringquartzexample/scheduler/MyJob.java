4218cd60dc7c7d01e31d5bd0fd51e5cd26d40604
ashishgajabi <ashish.gajabi@gmail.com>
2017-04-17 20:06:43 +1000

    Spring Quartz Scheduler - Using MethodInvokingJobDetailFactoryBean

---
+++ src/main/java/in/blogspot/ashish4java/simplespringquartzexample/scheduler/MyJob.java
@@ -0,0 +1,10 @@
+package in.blogspot.ashish4java.simplespringquartzexample.scheduler;
+
+public class MyJob {
+
+	
+	public void task() {
+		System.out.println("Task execution in progress...");
+	}
+
+}
